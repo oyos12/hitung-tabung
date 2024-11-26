eb118dd936a2947ff27d7ce02ceb2f8eed0e1f7f
oyos12 <kholish47@gmail.com>
2024-11-26 16:28:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit as st
 import math, time
 
 st.title("Menghitung :blue[Volume Tabung] :rocket:")
+
+r = st.number_input("Masukkan Jari-Jari (cm): ",0)
+t = st.number_input("Masukkan Tinggi (cm): ",0)
