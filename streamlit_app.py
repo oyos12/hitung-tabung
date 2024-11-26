b0a986d7ef36553a62f6c2f38aa42d6d5e69d769
oyos12 <kholish47@gmail.com>
2024-11-26 16:31:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ st.title("Menghitung :blue[Volume Tabung] :rocket:")
 
 r = st.number_input("Masukkan Jari-Jari (cm): ",0)
 t = st.number_input("Masukkan Tinggi (cm): ",0)
+
+if st.button("Hitung Volume", type="primary"):
+  loading = st.progress(0)
+  for i in range(100):
+    time.sleep(0.01)
+    loading.progress(i+1)
